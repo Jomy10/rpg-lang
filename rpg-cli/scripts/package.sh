950d6c74370b19e0459d4fb39744573f88de1e80
Jomy10 <jonas.vbs4@gmail.com>
2022-01-24 01:42:55 +0100

    Fixed gzip script

--- rpg-cli/scripts/package.sh
+++ rpg-cli/scripts/package.sh
@@ -3,10 +3,10 @@
 echo "Bundling binaries..."
 
 cp "target/aarch64-unknown-linux-gnu/release/rpg-cli" "build/linux-aarch64/rpgc"
-tar -czf "build/linux-aarch64.tar.gz" "build/linux-aarch64/rpgc"
+tar -czf "build/linux-aarch64.tar.gz" -C "build/linux-aarch64" rpgc
 
 cp "target/x86_64-pc-windows-gnu/release/rpg-cli.exe" "build/windows-x86_64/rpgc.exe"
-tar -czf "build/windows-x86_64.tar.gz" "build/windows-x86_64/rpgc.exe"
+tar -czf "build/windows-x86_64.tar.gz" -C "build/windows-x86_64" rpgc.exe
 
 cp "target/x86_64-apple-darwin/release/rpg-cli" "build/macos-x86_64/rpgc"
-tar -czf "build/macos-x86_64.tar.gz" "build/macos-x86_64/rpgc"
+tar -czf "build/macos-x86_64.tar.gz" -C "build/macos-x86_64" rpgc
